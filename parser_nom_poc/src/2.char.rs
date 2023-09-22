c2703277ea2c0221ddfdd8490546a84509e20f88
Akash Soni <33283321+akashsoni01@users.noreply.github.com>
2023-09-23 01:37:19 +0530

    char and is_a combinator added

--- parser_nom_poc/src/2.char.rs
+++ parser_nom_poc/src/2.char.rs
@@ -3,13 +3,17 @@ use nom::{
     character::complete::char,
 };
 
-fn parser(i: &str, input_char: char) -> IResult<&str, char> {
+fn parser(i: &str) -> IResult<&str, char> {
+    char('a')(i)
+}
+
+fn parser2(i: &str, input_char: char) -> IResult<&str, char> {
     char(input_char)(i)
 }
 
 fn main() {
     let input = "abcde";
-    match parser(input, 'e') { // 'a' error 'b', '', 'd', 'e'
+    match parser2(input, 'e') { // 'a' error 'b', '', 'd', 'e'
         Ok((remaining, parsed)) => {
             println!("parsed string was = {}", parsed);
             println!("remaining string is = {}", remaining);
@@ -18,4 +22,11 @@ fn main() {
             println!("Failed to parse {}", input);
         }
     };
+
+// other test case 
+assert_eq!(parser("abc"), Ok(("bc", 'a')));
+assert_eq!(parser(" abc"), Err(Err::Error(Error::new(" abc", ErrorKind::Char))));
+assert_eq!(parser("bc"), Err(Err::Error(Error::new("bc", ErrorKind::Char))));
+assert_eq!(parser(""), Err(Err::Error(Error::new("", ErrorKind::Char))));
+
 }
