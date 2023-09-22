3638deadab9bd6a99c088a38a837ffd35e94de9e
Akash Soni <33283321+akashsoni01@users.noreply.github.com>
2023-09-23 02:42:00 +0530

    module tests wip

---
+++ module_poc/tests/player_tests.rs
@@ -0,0 +1,18 @@
+/*
+// mod player;
+
+#[cfg(test)]
+mod tests {
+    // Import the module to be tested
+    use super::my_module;
+
+
+    #[test]
+    fn test_add() {
+
+        // Use the functions from the imported module for testing
+        // assert_eq!(my_module::add(2, 3), 5);
+        // assert_eq!(my_module::add(-1, 1), 0);
+    }
+}
+*/
