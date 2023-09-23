6a1ba3c6506d193b55906eac73bfd64b178ec684
Akash Soni <33283321+akashsoni01@users.noreply.github.com>
2023-09-23 15:44:48 +0530

    non of fixed

--- parser_nom_poc/src/6.none_of.rs
+++ parser_nom_poc/src/6.none_of.rs
@@ -2,23 +2,23 @@ use nom::{
     IResult,
     error::{Error, ErrorKind},
     Err,
-    bytes::complete::tag
+    character::complete::none_of,
 };
 
-fn parser(s: &str) -> IResult<&str, &str> {
-    tag("Hello")(s)
-  }
-  
-  /// |input=Hello, World! testing|Recognize=Hello|Parsed=Hello|Remaining=, World! testing|
-  /// |input=Something|Failed to parse|
-  /// |input=|Failed to parse|
-  /// |input=|Recognize=Hello|Failed to parse|
+// opposit of one of.
+fn parse_not_punctuation(input: &str) -> IResult<&str, char> {
+    none_of(".,!?")(input)
+}
+
+/// |input=Hello, world!|Recognize=.,!?||Parsed=H|Remaining=ello, world!|
+/// |input=,Hello, world!|Failed to parse||
+/// 
 
   fn main() {
-    let input = "";
-    let recognize_string:&str = "Hello"; // 
+    let input = ",Hello, world!";
+    let recognize_string:&str = ".,!?|"; // 
     let recognize_char: char = ' ';
-    match parser(input) { 
+    match parse_not_punctuation(input) { 
         Ok((remaining, parsed)) => {
             println!();
             if !recognize_char.is_whitespace() {
@@ -33,13 +33,7 @@ fn parser(s: &str) -> IResult<&str, &str> {
         }
         Err(_) => {
             println!();
-            if !recognize_char.is_whitespace() {
-                print!("|input={}|Recognize Char={}|Failed to parse|", input, recognize_char);
-            } else if recognize_string.is_empty() {
-                print!("|input={}|Failed to parse|", input);
-            } else {
-                print!("|input={}|Recognize={}|Failed to parse|", input, recognize_string);
-            }
+            print!("|input={}|Failed to parse||", input);
             println!();
             println!();
         }
@@ -49,43 +43,7 @@ fn parser(s: &str) -> IResult<&str, &str> {
 
 #[test]
 fn test_parser() {
-    assert_eq!(parser("Hello, World!"), Ok((", World!", "Hello")));
-    assert_eq!(parser("Something"), Err(Err::Error(Error::new("Something", ErrorKind::Tag))));
-    assert_eq!(parser(""), Err(Err::Error(Error::new("", ErrorKind::Tag))));
-
-}
-
-
-/*
-  fn parse_tag(input: &[u8], tag_to_parse: &[u8]) -> IResult<&[u8], &[u8]> {
-    tag(tag_to_parse)(input)
-}
-
-fn main() {
-    let input = b"Hello, World!";
-    let tag_to_parse = b"Hello";
-
-    match parse_tag(input, tag_to_parse) {
-        Ok((rest, result)) => {
-            println!("Successfully parsed: {:?}", result);
-            println!("Remaining input: {:?}", rest);
-        }
-        Err(err) => {
-            println!("Parsing error: {:?}", err);
-        }
+    assert_eq!(none_of::<_, _, (&str, ErrorKind)>("abc")("z"), Ok(("", 'z')));
+    assert_eq!(none_of::<_, _, (&str, ErrorKind)>("ab")("a"), Err(Err::Error(("a", ErrorKind::NoneOf))));
+    assert_eq!(none_of::<_, _, (&str, ErrorKind)>("a")(""), Err(Err::Error(("", ErrorKind::NoneOf))));
     }
-}
-
-
-In this example, tag(b"Hello")
-parses the byte slice "Hello" from the input b"Hello, World!", 
-and the remaining input is b", World!".
-You can use tag to search for and parse specific byte sequences in binary data.
-
-
-
-
-*/
-
-
-
