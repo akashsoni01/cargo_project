9d9d615465490ddbef18bb65be7ab2f98b6d6494
Akash Soni <33283321+akashsoni01@users.noreply.github.com>
2023-09-25 17:00:49 +0530

    take until

--- parser_nom_poc/src/main.rs
+++ parser_nom_poc/src/main.rs
@@ -2,32 +2,27 @@ use nom::{
     IResult,
     error::{Error, ErrorKind},
     Err,
-    bytes::complete::{take_till, take_till1}
+    bytes::complete::{take_until, take_until1}
 };
 
 
-/// empty allowed wihle. atleast on take is mandatory in take_till1
-/// |input=":Hello, world:some string"|Recognize=,|Parsed=""|Remaining=":Hello, world:some string"|
-/// |input="Hello, world:some string"|Recognize=,|Parsed="Hello, world"|Remaining=":some string"|
-fn till_colon(s: &str) -> IResult<&str, &str> {
-    take_till(|c| c == ':')(s)
+fn until_eof(s: &str) -> IResult<&str, &str> {
+    take_until("eof")(s) // matching a tag while till match a condition
+  }
+  
+/// |input="hello, world"|Recognize=eof|Failed to parse|
+/// 
+fn until_eof1(s: &str) -> IResult<&str, &str> {
+    take_until("eof")(s)
   }
 
-  /// |input=",Hello world:some string"|Recognize=,|Failed to parse|
-  /// |input="Hello, world:some string"|Recognize=,|Parsed="Hello"|Remaining=", world:some string"|
-  /// |input="Hello world:some string"|Recognize=,|Parsed="Hello world:some string"|Remaining=""|
-  /// 
-  fn till_comma(s: &str) -> IResult<&str, &str> {
-    take_till1(|c| c == ',')(s)
-  }
-
-
-
+/// |input="eofHello, world:some string"|Recognize=eof|Parsed=""|Remaining="eofHello, world:some string"|
+/// |input="hello, eofworld"|Recognize=eof|Parsed="hello, "|Remaining="eofworld"|
   fn main() {
-    let input = "Hello, world:some string";
-    let recognize_string:&str = ","; // 
+    let input = "hello, eofworld";
+    let recognize_string:&str = "eof"; // 
     let recognize_char: char = ' ';
-    match till_colon(input) { 
+    match until_eof1(input) { 
         Ok((remaining, parsed)) => {
             println!();
             if !recognize_char.is_whitespace() {
@@ -58,112 +53,79 @@ fn till_colon(s: &str) -> IResult<&str, &str> {
 
 #[test]
 fn test_parser() {
-    assert_eq!(till_colon("latin:123"), Ok((":123", "latin")));
-    assert_eq!(till_colon(":empty matched"), Ok((":empty matched", ""))); //allowed
-    assert_eq!(till_colon("12345"), Ok(("", "12345")));
-    assert_eq!(till_colon(""), Ok(("", "")));
-            
+    assert_eq!(until_eof("hello, worldeof"), Ok(("eof", "hello, world")));
+    assert_eq!(until_eof("hello, world"), Err(Err::Error(Error::new("hello, world", ErrorKind::TakeUntil))));
+    assert_eq!(until_eof(""), Err(Err::Error(Error::new("", ErrorKind::TakeUntil))));
+    assert_eq!(until_eof("1eof2eof"), Ok(("eof2eof", "1")));
+                
 }
+
 
 
 /*
+In the context of the `nom` library in Rust, both `take_until` and `take_till` are used to parse a sequence of bytes until a specified condition is met. However, there is a subtle difference between them:
 
-Sure! Imagine you have a big box of colorful building blocks, 
-and you want to pick some of them.
-But you have a special rule: you can only pick blocks until you find a block of a certain color. 
-Let's say you're looking for a blue block.
+1. **`take_until`**: This combinator parses bytes until it encounters a specific byte sequence (a tag). It stops parsing when it finds the specified tag and includes the tag in the result.
 
-take_till: With "take_till," you start picking blocks one by one. 
-You keep picking blocks until you find a blue block. 
-When you find the blue block, you stop picking. 
-If there are no blue blocks, you keep picking until you've looked at all the blocks.
+   For example, if you use `take_until("abc")`, it will parse until it finds the first occurrence of "abc" and include "abc" in the result.
 
-take_till1: "take_till1" is similar, but it has a rule that says you must pick at least one block before you stop. 
+   ```rust
+   use nom::{
+       bytes::complete::take_until,
+       IResult,
+   };
 
-So, if you see a blue block right away, you pick it and stop. But if you don't see a blue block, 
-you keep picking until you find one.
+   fn main() {
+       let input = b"Hello, abc World!";
+       
+       match parse_take_until(input) {
+           Ok((rest, result)) => {
+               println!("Successfully parsed: {:?}", result);
+               println!("Remaining input: {:?}", rest);
+           }
+           Err(err) => {
+               println!("Parsing error: {:?}", err);
+           }
+       }
+   }
 
-take_till_m_n: "take_till_m_n" is a bit different. It's like having two rules. 
-One rule says you have to pick at least a certain number of blocks, like 2 blocks. 
-The other rule says you stop when you find a blue block or when you've picked a certain number of blocks, like 5 blocks. 
-So, you might pick 2, 3, 4, or 5 blocks, 
-but you'll stop when you find a blue block or when you've picked 5 blocks.
+   fn parse_take_until(input: &[u8]) -> IResult<&[u8], &[u8]> {
+       take_until("abc")(input)
+   }
+   ```
 
-These helpers help you pick blocks in a special way, 
-following your rules about finding blue blocks. 
-It's like playing a game with your blocks and looking for the blue ones!
+   In this example, `parse_take_until` uses `take_until("abc")` to parse until it finds "abc," and "abc" is included in the result.
 
+2. **`take_till`**: This combinator parses bytes until a specified condition is met, based on a provided closure. It stops parsing when the closure's condition is `true` and does not include the byte that satisfies the condition in the result.
 
+   For example, if you use `take_till(|c| c == b',')`, it will parse until it finds a comma (`,`), but the comma won't be included in the result.
 
+   ```rust
+   use nom::{
+       bytes::complete::take_till,
+       IResult,
+   };
 
+   fn main() {
+       let input = b"Hello, World!";
+       
+       match parse_take_till(input) {
+           Ok((rest, result)) => {
+               println!("Successfully parsed: {:?}", result);
+               println!("Remaining input: {:?}", rest);
+           }
+           Err(err) => {
+               println!("Parsing error: {:?}", err);
+           }
+       }
+   }
 
+   fn parse_take_till(input: &[u8]) -> IResult<&[u8], &[u8]> {
+       take_till(|c| c == b',')(input)
+   }
+   ```
 
+   In this example, `parse_take_till` uses `take_till(|c| c == b',')` to parse until it finds a comma (`,`), but the comma is not included in the result.
 
-take_till1: Similar to take_till, but it requires at least one byte to satisfy the condition.
-
-use nom::{
-    bytes::complete::take_till1,
-    IResult,
-};
-
-fn main() {
-    let input = b"Hello, World!";
-    
-    match parse_take_till1(input) {
-        Ok((rest, result)) => {
-            println!("Successfully parsed: {:?}", result);
-            println!("Remaining input: {:?}", rest);
-        }
-        Err(err) => {
-            println!("Parsing error: {:?}", err);
-        }
-    }
-}
-
-fn parse_take_till1(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    take_till1(|c| c == b',')(input)
-}
-
-
-
-
-
-
-
-
-
-take_till_m_n: This combinator allows you to specify a minimum (m) and maximum (n)
- number of bytes to take that satisfy the condition.
-use nom::{
-    bytes::complete::take_till_m_n,
-    IResult,
-};
-
-fn main() {
-    let input = b"Hello, World!";
-    
-    match parse_take_till_m_n(input) {
-        Ok((rest, result)) => {
-            println!("Successfully parsed: {:?}", result);
-            println!("Remaining input: {:?}", rest);
-        }
-        Err(err) => {
-            println!("Parsing error: {:?}", err);
-        }
-    }
-}
-
-// not available 
-fn parse_take_till_m_n(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    take_till_m_n(2, 5, |c| c == b',')(input)
-}
-
-In this example, parse_take_till_m_n uses take_till_m_n to parse between 2 to 5 bytes until a comma (,). 
-The parsed bytes can be "Hello" or "Hello,".
-These are some related parsing functions that you can use with take_till in nom. 
-They allow you to customize the parsing behavior based on your specific needs.
-
-
-
-
+So, the main difference is in what they consider as the stopping point: `take_until` stops at a specific tag and includes it, while `take_till` stops based on a condition you define and does not include the character that satisfies the condition.
 */
