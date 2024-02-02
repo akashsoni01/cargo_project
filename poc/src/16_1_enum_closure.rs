f57619a49d6e483c49190a623b65eeb4f693a1f1
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2024-02-02 20:50:36 +0530

    Update 16_1_enum_closure.rs

--- poc/src/16_1_enum_closure.rs
+++ poc/src/16_1_enum_closure.rs
@@ -20,3 +20,35 @@ impl YourEnum {
     }
   }
 }
+
+
+// usecase with generic data type 
+pub fn is_sorted<I>(items: I, direction: YourEnum) -> bool
+where
+  I: IntoIterator,
+  I::Item: Ord + Clone,
+{
+  items
+    .into_iter()
+    .tuple_windows()
+    .all(direction.as_closure())
+}
+
+#[test]
+fn test_is_sort_assertion() {
+  let a = vec![1, 20, 3];
+  let b = vec![1, 2, 60];
+  let c = vec![100, 20, 6];
+  let d = vec![100, 20, 60];
+  assert!(!is_sorted(a, YourEnum::CASEONE));
+  assert!(is_sorted(b, YourEnum::CASEONE));
+  assert!(is_sorted(c, YourEnum::CASETOW));
+  assert!(!is_sorted(d, YourEnum::CASETOW))
+}
+
+
+// other application 
+/*
+  assert!(is_sorted(b, Direction::ASC));
+  assert!(is_sorted(c, Direction::DESC));
+*/
