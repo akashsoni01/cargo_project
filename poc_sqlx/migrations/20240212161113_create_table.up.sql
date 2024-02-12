46074e2e8e7328281fe71d11ad289a567a6417ca
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2024-02-12 22:36:46 +0530

    migration up - run down - revert

--- poc_sqlx/migrations/20240212161113_create_table.up.sql
+++ poc_sqlx/migrations/20240212161113_create_table.up.sql
@@ -1,2 +1,5 @@
--- Add up migration script here
-DROP TABLE IF EXISTS my_table;
+CREATE TABLE IF NOT EXISTS my_table (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- poc_sqlx/src/main.rs
+++ poc_sqlx/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/*
+sqlx database create
+sqlx database drop
+
+sqlx migrate add <name>
+
+sqlx migrate run
+
+*/ 
+
+/*
+sqlx database create
+sqlx database drop
+
+sqlx migrate add -r your_table_or_migration_name
+
+sqlx migrate run
+sqlx migrate revert
+
+*/ 
+
+/*
+very important command to ofline your query and to check the syntax of your query
+cargo sqlx prepare --workspace -- --all-targets --all-features
+
+*/
