ef9318ec3690f53630c76062268b4d93a922d92b
akash <akashsoni8285@gmail.com>
2024-05-10 15:22:54 +0530

    comment removed

--- poc/src/35_refrence.rs
+++ poc/src/35_refrence.rs
@@ -1,14 +1,6 @@
 
 /*
-fn main() {
-    multi_line_string_with_new_line();
-}
-fn multi_line_string_with_new_line() {
-    let multi_line = r#"This is a
-    multi line string"#;
-
-    println!("{}", multi_line);
-}
+https://dhghomon.github.io/easy_rust/Chapter_16.html
 */
 
 fn main() {
